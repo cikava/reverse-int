23ef983e78aba28e54ae18d42ee4fe9b58123be2
cikava <hembitski@outlook.com>
2022-06-30 20:52:35 +0300

    feat: Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let s = '' + Math.abs(n);
+    let str = '';
+    let i = s.length;
+    while (i > 0){
+      i = i - 1;
+      str = str + s[i];
+    }
+return(+str);
 }
